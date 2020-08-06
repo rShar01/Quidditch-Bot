738bbfc5715cccf734bf439e5dee7f3f0b8d6e8e
rShar01 <ryan.shar01@gmail.com>
2020-08-06 07:52:18 -0700

    First Commit

--- Commands/attack.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'attack',
-	description: 'do not run this one',
-	execute(message, args) {
-		for(let i = 0; i < 25; i++) {
-			message.author.send('Fuck you\n');
-		}
-	},
-};
