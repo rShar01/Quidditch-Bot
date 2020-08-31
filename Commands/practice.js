58044c842da311e394dfc1244a7c558760fdb63f
rShar01 <49721773+rShar01@users.noreply.github.com>
2020-08-31 10:49:20 -0700

    Add files via upload

---
+++ Commands/practice.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'practice',
+	description: 'When are practices?',
+	execute(message) {
+		message.channel.send('Still TBD! We will update you as we learn more about the current situation!');
+	},
+};
